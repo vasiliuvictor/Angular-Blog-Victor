f8a6ea48db0644f83b70110b0bb0968fafdc6eaf
victor.vasiliu <victor.vasiliu@astory.tech>
2025-07-23 17:06:55 +0300

    added host for firebase and deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { environment } from '../environments/environment';
-import { CarouselModule } from 'ngx-owl-carousel-o';
-
-import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    CarouselModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
 export class AppModule { }
